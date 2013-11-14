786bdbe01d7b5b7940ee969ddf6d641534264330
Steve Zhu <st.zhu1@gmail.com>
2013-11-14 12:05:17 -0500

    Basic app.py implementation

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import utils
+
+app = Flask(__name__)
+app.secret_key = ''
+
+env = app.jinja_env
+env.line_statement_prefix = '='
+env.globals.update(utils=utils)
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host='0.0.0.0', port=5006)
