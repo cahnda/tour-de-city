496e5971ab845a734d50a814fa39880e1f49764f
Steve Zhu <st.zhu1@gmail.com>
2013-11-15 23:30:55 -0500

    Corrected importing of py files

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
-import utils
+from flask import Flask, render_template
+from py import *
 
 app = Flask(__name__)
-app.secret_key = ''
+app.config.from_object('py.config')
 
 env = app.jinja_env
 env.line_statement_prefix = '='
 env.globals.update(utils=utils)
 
 if __name__ == '__main__':
-	app.debug = True
 	app.run(host='0.0.0.0', port=5007)
--- py/__init__.py
+++ py/__init__.py
@@ -1,9 +1,1 @@
-from flask import Flask
-import citi_bike, google_maps, google_places, utils
-
-__all__ = ['citi_bike', 'google_maps', 'google_places', 'utils']
-app = Flask(__name__)
-app.config.from_object('config')
-
-if __name__ == '__main__':
-	app.run()
+__all__ = ['citi_bike', 'google_maps', 'google_places', 'utils', 'config']
