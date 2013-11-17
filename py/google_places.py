4c2187265e0a2a9a2cf1f7c82d426818a87a9e82
David Cahn <cahnda@gmail.com>
2013-11-17 17:15:02 -0500

    Functional google places

--- py/google_places.py
+++ py/google_places.py
@@ -6,36 +6,37 @@ from urllib2 import urlopen
 
 def findPlaces (latitude, longitude, responses):
     #make request
-    AUTH_KEY = 'AIzaSyBCHXuWpnzODuPRa4-CLTtC9JPyZWEFxPo'
+    AUTH_KEY = 'AIzaSyC-Rd4Mhjt7PPqMHGDjZdBJp3W835STm5w'
     LOCATION = str (latitude) + "," + str (longitude)
-    RADIUS = '3500' #in meters, approx. 2 miles
+    RADIUS = 3500 #in meters, approx. 2 miles
     RANKBY = 'prominence'
     KEYWORDS = 'tourism+monument'
-    SENSOR = false
     TYPES = ''
 
     # ADD USER INPUT FOR WHAT TYPE OF LOCATION THEY ARE LOOKING FOR
-    try: 
+    if responses == []:
+        TYPES = 'amusement_park|aquarium|art_gallery|bowling_alley|cafe|city_hall|establishment|library|museum|night_club|park|restaurant|shopping_mall|store|stadium|store'
+    else:
         for word in responses:
             TYPES = TYPES + str (word) + '|'
-        TYPES = TYPES [:-1]
-    except:         
-        TYPES = 'amusement_park+aquarium+art_gallery+bowling_alley+cafe+city_hall+establishment+library+museum+night_club+park+restaurant+shopping_mall+store+stadium+store'
+        TYPES = TYPES [:-1]       
             
-    url = ('https://maps.googleapis.com/maps/api/place/search/json?location=%s'
-           '?keyword=%s?types=%s&radius=%s&sensor=false&key=%s') % (LOCATION, KEYWORDS, TYPES, RADIUS, AUTH_KEY)
-    
+    url = ('https://maps.googleapis.com/maps/api/place/search/json'
+           '?keyword=%s?types=%s&location=%s&radius=%s&sensor=false&rankby=%s&key=%s') %  (KEYWORDS, TYPES, LOCATION, RADIUS, RANKBY, AUTH_KEY)
+   
     print url
 
     # Send the GET request to the Place details service (using url from above)
-    response = urllib2.urlopen(url)
-    
+    response = urlopen(url)
+     
     # Get the response and use the JSON library to decode the JSON
     json_raw = response.read()
     json_data = json.loads(json_raw)
 
     # Iterate through the results and print them to the console
-   # if json_data[‘status’] == ‘OK’:
-    #    for place in json_data['results']:
-     #       print ‘%s: %s\n’ % (place['name'], place['reference'])'
-            
+    if json_data['status'] == 'OK':
+        for place in json_data['results']:
+            print '\n %s: %s  \n Rating: %s' % (place['name'], place ['vicinity'], place['rating'])           
+
+if __name__ == '__main__':
+    findPlaces (40.7472569628042, -73.99085998535156, [])
