26cc2d2ad2d299a9bf04334aa2a0758c6c693b53
Severyn Kozak <severyn.kozak@gmail.com>
2013-11-18 22:52:07 -0500

    Add main/err routes to app; add template content.
    Add:
    	app.py
    		-add route for main page, "index.html", and two handlers
    		for 404 and 500 errors that load error/(404, 500).html
    		respectively.
    
    	templates/layout.html
    		-add favicon link and changed {{title}} to "block title"; instead of
    		manually passing in a value from app.py, we'll inherit a block
    		from child templates.
    
    	templates/errors
    		-to contain all error pages.

--- app.py
+++ app.py
@@ -8,5 +8,17 @@ env = app.jinja_env
 env.line_statement_prefix = '='
 env.globals.update(utils=utils)
 
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.errorhandler(404)
+def error404(error):
+	return render_template("errors/404.html"), 404
+
+@app.errorhandler(500)
+def error500(error):
+	return render_template("errors/500.html"), 500
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5007)
