61186eab6322f537e8a2d703b0af21d83ac4b208
Severyn Kozak <severyn.kozak@gmail.com>
2013-11-20 18:23:22 -0500

    Add long/latitude, fxn for nearest bike depot.
    Add:
    	py/citi_bike.py
    		-add function that intakes a longitude and latitude, and returns
    		the address of the nearest bike depot.
    
    	py/utils.py
    		-add function to return all Mongo city_bike collection's
    		documents.
    Mod:
    	py/citi_bike.py
    		-bike depot documents sent to Mongo now contain longitude and latitude,
    		in addition to an address.

--- py/citi_bike.py
+++ py/citi_bike.py
@@ -1,11 +1,29 @@
-import json
+import json, utils
 from urllib2 import urlopen
 
 CITI_BIKE_JSON_URL = "http://citibikenyc.com/stations/json"
 
+def nearest_station_address(lat, lon):
+	stations = utils.citi_bike_stations()
+
+	nearest_station = stations[0]
+	shortest_dist = distance(nearest_station["lat"], nearest_station["lon"], lat, lon)
+
+	for station in stations[1:]:
+		dist = distance(station["lat"], station["lon"], lat, lon)
+		if dist < shortest_dist:
+			nearest_station = station
+			shortest_dist = dist
+
+	return nearest_station["address"]
+
 def station_addresses():
 	citi_stations = get_json()["stationBeanList"]
-	return [{"address": station["stationName"]} for station in citi_stations]
+	return [{\
+		"address": station["stationName"],\
+		"longitude": station["longitude"],\
+		"latitude": station["latitude"]\
+	} for station in citi_stations]
 
 def get_json():
 	citi_url = urlopen(CITI_BIKE_JSON_URL)
