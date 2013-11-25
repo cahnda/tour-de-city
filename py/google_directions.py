15067428bfe72554585c50616eb6ec914e46a25d
Severyn Kozak <severyn.kozak@gmail.com>
2013-11-24 22:27:32 -0500

    Fix bike Mongo bug; add GoogleDir optimized route.
    Add:
    	py/google_directions.py
    		-add module for interaction with the Google Directions API;
    		currently contains a single function, get_waypoint_order, which
    		returns an ordered list of waypoints optimized by distance.
    
    Fix:
    	py/utils.py
    		-add a statement to remove all pre-existing citi-bike depot
    		documents from the citi_bike collection in our Mongo database,
    		which wouldn't have been replaced by the insertion of new data
    		with identical keys (different hash IDs).

---
+++ py/google_directions.py
@@ -0,0 +1,13 @@
+from urllib2 import urlopen
+from json import loads
+
+API_URL = ("http://maps.googleapis.com/maps/api/directions/json?" 
+          "origin=%s"
+		  "&waypoints=optimize:true|%s"
+          "&destination=%s"
+          "&sensor=false")
+
+def get_waypoint_order(origin, waypoints, destination):
+	query_url = API_URL % (origin, "|".join(waypoints), destination)
+	directions_json = loads(urlopen(query_url).read())
+	return directions_json["routes"][0]["waypoint_order"]
