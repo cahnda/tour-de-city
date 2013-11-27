a89c1457808c024492adb2e265fa1612a968dad7
David Cahn <cahnda@gmail.com>
2013-11-27 12:33:56 -0500

    Changes to index and app.py to integrate home page with flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from py import *
 
 app = Flask(__name__)
@@ -8,15 +8,22 @@ env = app.jinja_env
 env.line_statement_prefix = '='
 env.globals.update(utils=utils)
 
-@app.route("/")
+@app.route("/", methods = ["GET","POST"])
 def index():
-        if request.method=="GET":
+        print 'on index page'
+        if request.method =="GET":
+                print 'get'
                 return render_template("index.html")
         else: 
-                return redirect ("makeTour")
+                print 'post'
+                button = request.form['button']
+                if button == "Submit":
+                       tmp =  request.form['tour']
+                       print tmp
+                       return tmp
 
-@app.selectionPage ("makeTour")
-def makeTour ():
+#@app.selectionPage ("makeTour")
+#def makeTour ():
                 
 @app.errorhandler(404)
 def error404(error):
