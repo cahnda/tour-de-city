d2d020ad547535200f67371e28413da79cfc911a
Severyn Kozak <severyn.kozak@gmail.com>
2013-11-30 13:25:58 -0500

    Add functional nearest_station fxn, with geo dist.
    Add:
    	py/citi_bike.py
    		-add fully functional nearest_station_address(street), which
    		intakes a street address string and returns the address of the
    		nearest Citi Bike depot. Use GeoPy to geocode user's address
    		into lon/lat coordinates; work around excessive use of internal
    		API key by geocoding only once.

--- py/citi_bike.py
+++ py/citi_bike.py
@@ -1,21 +1,30 @@
 import json, utils
 from urllib2 import urlopen
 
+from geopy.distance import vincenty    # geodesic distance
+from geopy.geocoders import GoogleV3
+
 CITI_BIKE_JSON_URL = "http://citibikenyc.com/stations/json"
 
-def nearest_station_address(lat, lon):
-	stations = utils.citi_bike_stations()
+def nearest_station_address(street):
+	stations = utils.get_citi_bike_stations()
+	street_coor = GoogleV3().geocode(street)[1]
 
 	nearest_station = stations[0]
-	shortest_dist = distance(nearest_station["lat"], nearest_station["lon"], lat, lon)
+	station_coor = (nearest_station["latitude"], nearest_station["longitude"])
+	shortest_dist = distance(street_coor, station_coor)
 
 	for station in stations[1:]:
-		dist = distance(station["lat"], station["lon"], lat, lon)
+		station_coor = (station["latitude"], station["longitude"])
+		dist = distance(street_coor, station_coor)
 		if dist < shortest_dist:
 			nearest_station = station
 			shortest_dist = dist
 
 	return nearest_station["address"]
+
+def distance(coor1, coor2):
+	return vincenty(coor1, coor2).miles
 
 def station_addresses():
 	citi_stations = get_json()["stationBeanList"]
