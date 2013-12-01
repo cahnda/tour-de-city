aff94c238d0d74c3ba3e55e92d406bb1efb98365
Steve Zhu <st.zhu1@gmail.com>
2013-11-30 22:30:15 -0500

    Added venue selection to new-home

--- static/js/maps.locationpicker.js
+++ static/js/maps.locationpicker.js
@@ -2,22 +2,35 @@ var locationPicker;
 (function($) {
 	// frame = dom object
 	locationPicker = function(frame) {
+		var $wrapper = $(frame).parent(),
+			$search = $wrapper.find('.search-form'),
+			$text = $wrapper.find('.search-text'),
+			$submit = $wrapper.find('.search-button'),
+			$latitude = $wrapper.find('input[name=latitude]'),
+			$longitude = $wrapper.find('input[name=longitude]');
+
 		var frameWindow = frame.contentWindow,
 			GMaps = frameWindow.GMaps,
 			map = frameWindow.map;
 		var marker = map.addMarker({
 			title: 'Current Location',
-			lat: 40.7143528,
-			lng: -74.0059731,
+			lat: $latitude.val(),
+			lng: $longitude.val(),
 			draggable: true
 		});
+
+		function updateLocation(loc) {
+			marker.setPosition(loc);
+			$latitude.val(loc.lat());
+			$longitude.val(loc.lng());
+		}
 
 		function performSearch(loc) {
 			GMaps.geocode({
 				address: loc,
 				callback: function(results, status) {
 					if (status == 'OK') {
-						marker.setPosition(results[0].geometry.location);
+						updateLocation(results[0].geometry.location);
 						map.panTo(results[0].geometry.location);
 					}
 				}
@@ -25,20 +38,25 @@ var locationPicker;
 		}
 
 		map.addListener('dblclick', function(event) {
-			marker.setPosition(event.latLng);
+			updateLocation(event.latLng);
 		});
 		marker.addListener('dragend', function(event) {
-			marker.setPosition(marker.position);
+			updateLocation(event.latLng);
 		});
 
-		var $wrapper = $(frame).parent(),
-			$search = $wrapper.find('.search-form'),
-			$text = $search.find('.search-text');
 
-		$search.submit(function() {
+		function submitSearch() {
 			var loc = $text.val();
 			performSearch(loc);
-			return false;
+		}
+
+		$submit.click(submitSearch);
+		$text.keypress(function(e) {
+			if(e.which == 13) {
+				submitSearch();
+				return false;
+			}
+			return true;
 		});
 	}
 }(jQuery));
