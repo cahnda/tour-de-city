7956f86f059e3a5d897121948ebb4dcb543d5296
David Cahn <cahnda@gmail.com>
2014-01-12 17:13:49 -0500

    fixed linking programs with javascript ui

--- templates/makeTour_script.js
+++ templates/makeTour_script.js
@@ -1,19 +1,11 @@
-
- <meta charset="utf-8">
-  <title>jQuery UI Dialog - Default functionality</title>
-  <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
-  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-  <link rel="stylesheet" href="/resources/demos/style.css">
-
-<script>
 window.onload = function () {
     alert ("loaded");
-    imgList = document.getElementById ("locationNum");
+    imgList = document.getElementsByClassName  ("location-content");
     console.log (imgList.length);
-    console.log (imgList);
-    imgList.onclick = function () {
-	$.modal("this is a modal").dialog();
+    for img in imgList {
+	console.log (imgList);
+	imgList.onclick = function () {
+	    $.modal("this is a modal");
+	};
     };
 };
-</script>
