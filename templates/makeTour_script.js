911cb3de0afde049e542b87b82cb7794b24db624
David Cahn <cahnda@gmail.com>
2014-01-12 13:51:49 -0500

    working with jquery user interface and testing various applications of dialogue dox

--- templates/makeTour_script.js
+++ templates/makeTour_script.js
@@ -1,9 +1,19 @@
+
+ <meta charset="utf-8">
+  <title>jQuery UI Dialog - Default functionality</title>
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+
+<script>
 window.onload = function () {
     alert ("loaded");
     imgList = document.getElementById ("locationNum");
     console.log (imgList.length);
     console.log (imgList);
     imgList.onclick = function () {
-	$.modal("this is a modal");
+	$.modal("this is a modal").dialog();
     };
 };
+</script>
