a66c1acf2bfe6b3e82598b61016bc09fea00b00d
David Cahn <cahnda@gmail.com>
2014-01-12 13:10:38 -0500

    Successfully implemented Modal.js

--- templates/makeTour_script.js
+++ templates/makeTour_script.js
@@ -1,4 +1,9 @@
-document.onload =  function () {
+window.onload = function () {
+    alert ("loaded");
     imgList = document.getElementById ("locationNum");
     console.log (imgList.length);
+    console.log (imgList);
+    imgList.onclick = function () {
+	$.modal("this is a modal");
+    };
 };
