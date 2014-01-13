316cba031f1332e344f908a8284eebbeca394301
David Cahn <cahnda@gmail.com>
2014-01-13 12:28:18 -0500

    combined dialogue and modular capability for images on page 2

---
+++ templates/.#makeTour_script.js
@@ -0,0 +1,1 @@
+student@ac02kp08zf67d.78m475.nycboe.org.11346
--- templates/makeTour_script.js
+++ templates/makeTour_script.js
@@ -1,11 +1,16 @@
-window.onload = function () {
-    alert ("loaded");
-    for (var i=1;i<={{locLen}};i++) {
-	strIt = i.toString();
-	divName = "myDiv" + strIt
-	img = document.getElementById(divName);
-	img.onclick = function () {
-	    $.modal("this is a modal");
-	};
-    };
+
+$(function() {
+    $( "#dialog" ).dialog({ modal: true });
 };
+
+  window.onload = function () {
+      alert ("loaded");
+      for (var i=1;i<={{locLen}};i++) {
+	  strIt = i.toString();
+	  divName = "myDiv" + strIt
+	  img = document.getElementById(divName);
+	  img.onclick = function () {
+	      $.modal("this is a modal")
+	  };
+      };
+  };
