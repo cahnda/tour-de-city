07d6990e41c124c1ce1c9e09786d48b0b55357ca
David Cahn <cahnda@gmail.com>
2014-01-14 18:52:15 -0500

    Correctly sized images corresponding to the correct click

--- templates/makeTour_script.js
+++ templates/makeTour_script.js
@@ -1,29 +1,42 @@
-<script>
 
 window.onload = function () {
     alert ("loaded");
+    document.getElementById("dialog").innerHTML = "";
+
     for (var i=1;i<={{locLen}};i++) {
 	strIt = i.toString();
 	divName = "myDiv" + strIt
-	img = document.getElementById(divName);
-	var iDiv = document.createElement('div');
-	iDiv.setAttribute("id", divName + "pop");
-	descriptionText = {{locs[i][4]}}
-	var txtNd=document.createTextNode(descriptionText);
-	var imgpop = document.createElement("img");
-	imgpop.setAttribute("src", "images/hydrangeas.jpg");
-	imgpop.setAttribute("height", "768");
-	imgpop.setAttribute("width", "1024");
+        var loc = JSON.parse('{{locs | tojson | safe}}');
+	imgTxt = loc[i-1][5];
+	phyImg = loc[i-1][3];
 
-	img.onclick = function () {
+	var imgSc = document.getElementById(divName);
+	imgSc.onclick = function () {
+	    var source = this.getElementsByTagName('img')[0];
+	    var phyImg = source.src;
+
+	    console.log (source);
+	    document.getElementById("dialog").innerHTML = "";
+	    var iDiv = document.getElementById("dialog");
+	    var txtNd=document.createTextNode(imgTxt);
+	    iDiv.appendChild(txtNd);
+
+	    var imgpop = document.createElement("img");
+	    imgpop.setAttribute("src", phyImg );
+	    imgpop.setAttribute("height", "500");
+	    imgpop.setAttribute("width", "500");
+
+	    iDiv.appendChild(imgpop);
+
 	    $("#dialog").dialog({
 		modal:true, 
+		title: 
+		height: "auto",
+		width: "auto",
 		buttons: {
 		    Ok: function() {
-			$(  this ).dialog( "close" );
+			$( this ).dialog( "close" );
 		    }}});
 	};
     };
 };
-
-</script>
