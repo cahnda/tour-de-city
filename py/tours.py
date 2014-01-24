8a264982ecba05f2ef71e8488605647e11a2b679
Victoria Greene <topaz.q98@gmail.com>
2014-01-23 23:29:17 -0500

    changed tour input to array of addresses

--- py/tours.py
+++ py/tours.py
@@ -4,17 +4,15 @@ client = pymongo.MongoClient()
 db = client.SSSD
 
 class Tour():
-    def __init__(self, a1, a2, a3, rate):
-        self.address1 = a1
-        self.address2 = a2
-        self.address3 = a3
+    def __init__(self, addresses, rate):
+        self.addresses = addresses
         self.rate = rate
         
     def __str__(self):
-        return "%s, %s, %s: %.1f"%(t.address1, t.address2, t.address3, t.rate)
+        return "%s: %.1f"%(t.addresses, t.rate)
 
 def form(t):
-    return "%s, %s, %s: %.1f"%(t.address1, t.address2, t.address3, t.rate)
+    return "%s: %.1f"%(t.addresses, t.rate)
 
 def average(tours):   #averages the rates of the tours with the same addresses (regardless of the order) and pops the duplicates 
     temp = list(tours)
@@ -24,7 +22,7 @@ def average(tours):   #averages the rates of the tours with the same addresses (
             counter = 1
             for t in range(x+1,len(temp)):
                 if t < len(temp):
-                    if ((temp[x].address1 == temp[t].address1 or temp[x].address1 == temp[t].address2 or temp[x].address1 == temp[t].address3) and (temp[x].address2 == temp[t].address1 or temp[x].address2 == temp[t].address2 or temp[x].address2 == temp[t].address3) and (temp[x].address3 == temp[t].address1 or temp[x].address3 == temp[t].address2 or temp[x].address3 == temp[t].address3)):
+                    if set(temp[x].addresses) == set(temp[t].addresses):
                         ratesum += temp[t].rate
                         counter += 1
                         temp.pop(t)
@@ -53,15 +51,15 @@ def getSorted():
 
 #-----------------------------Tests-----------------------------------
 
-"""t = Tour("address 1", "address 2", "address 3", 4.5)
+"""t = Tour(["address 1", "address 2", "address 3"], 4.5)
 
-t1 = Tour("this", "that", "the other", 3.0)
+t1 = Tour(["this", "that", "the other"], 3.0)
 
-t2 = Tour("good", "hello", "bye", 3.79230493048302)
+t2 = Tour(["good", "hello", "bye"], 3.79230493048302)
 
-t3 = Tour("hello", "good", "bye", 5.0)
+t3 = Tour(["hello", "good", "bye"], 5.0)
 
-t4 = Tour("that", "the other", "this", 2.0)
+t4 = Tour(["that", "the other", "this"], 2.0)
 
 tours = [t,t1,t2,t3,t4]
 
