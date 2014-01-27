dc6a9153299d08fa35ae6bc563495c058ab92ba3
Severyn Kozak <severyn.kozak@gmail.com>
2014-01-27 13:18:40 -0500

    Clean up spin.js config variable dict.
    Mod:
    	static/js/google_oauth.js
    		-condense spin.js configuration variable dictionary (delete comments,
    		declare multiple key-value pairs on single lines).
    		-increase speed/trail of spin animation

--- static/js/google_oauth.js
+++ static/js/google_oauth.js
@@ -4,22 +4,10 @@
 
 $("#gSignInContainer").click(function() {
 	var opts = {
-		lines: 15, // The number of lines to draw
-		length: 9, // The length of each line
-		width: 2, // The line thickness
-		radius: 10, // The radius of the inner circle
-		corners: 1, // Corner roundness (0..1)
-		rotate: 0, // The rotation offset
-		direction: 1, // 1: clockwise, -1: counterclockwise
-		color: '#fff', // #rgb or #rrggbb or array of colors
-		speed: 1.0, // Rounds per second
-		trail: 60, // Afterglow percentage
-		shadow: false, // Whether to render a shadow
-		hwaccel: false, // Whether to use hardware acceleration
-		className: 'spinner', // The CSS class to assign to the spinner
-		zIndex: 2e9, // The z-index (defaults to 2000000000)
-		top: 'auto', // Top position relative to parent in px
-		left: 'auto' // Left position relative to parent in px
+		lines: 15, length: 9, width: 2, radius: 10, corners: 1, rotate: 0,
+		direction: 1, color: '#fff', speed: 2.2, trail: 70, shadow: false,
+		hwaccel: false, className: 'spinner', zIndex: 2e9, top: 'auto',
+		left: 'auto'
 	};
 	window.spinner = new Spinner(opts).spin();
 	$(this).find("#gSignInWrapper").hide();
