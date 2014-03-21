cd59ba23806746095b08226bbb0b9110a3fc375c
David Cahn <cahnda@gmail.com>
2014-03-20 21:02:12 -0400

    Commiting Project to Heroku

---
+++ py/config.py
@@ -0,0 +1,2 @@
+DEBUG = True
+SECRET_KEY = "]c<jcu1gqcS6@pE*2Qk&CdhG}HB@y6q.3T5c2RQ*G7K=N_vWu7R''`E4Y10JU(Ap"
---
+++ static/js/show_directions.js
@@ -0,0 +1,43 @@
+$(window).load(function() {
+
+      $("#map-button").click(function(){
+            if($(this).hasClass("click")){
+                  $(this).removeClass("click").text("View Tour");
+                  $("#final_map").fadeOut();
+            }
+            else {
+                  $(this).addClass("click").text("Hide Tour");
+                  $("#final_map").fadeIn();       
+            }
+      });
+
+      //Other
+      $("#final_map").hide();
+
+      //Google Maps
+      var fwindow = $('iframe')[0].contentWindow;
+      var fdocument = fwindow.document
+      google = fwindow.google;
+      map = fwindow.map.map;
+
+      directionsService = new google.maps.DirectionsService();
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      directionsDisplay.setMap(map);
+      directionsDisplay.setPanel(fdocument.getElementById('directions-panel'));
+      calcRoute();
+});
+
+$(document).ready(function(){
+	$("#rate-up, #rate-down").click(function(){
+		var rate_json = {
+			"rate_value" : this.value
+		};
+		$.ajax({
+			type : "POST",
+			url : "/tour=" + document.URL.split("tour=")[1],
+			data : JSON.stringify(rate_json, null, '\t'),
+			contentType : "application/json;charset=UTF-8",
+		});
+		$("#rate-up, #rate-down").fadeOut()
+	})
+})
