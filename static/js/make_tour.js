da39d148b5378207ad976f3cb48e5090f52f2cd0
Severyn Kozak <severyn.kozak@gmail.com>
2014-03-25 19:50:05 -0400

    Attempt to fix location deselection.
    Fix:
    	static/js/make_tour.js
    		-attempt fixing location deselection.

--- static/js/make_tour.js
+++ static/js/make_tour.js
@@ -5,31 +5,32 @@ $(document).ready(function(){
 	// click events for .location-container and .dialog buttons
 	$(".location-container").each(function(){
 
-	var locationSelector = ".location-container#" + this.id + " label";
-	var dialogSelector = ".dialog#" + this.id;
+		var locationSelector = ".location-container#" + this.id + " label";
+		var dialogSelector = ".dialog#" + this.id;
 
-	$(this).click(function(){
-		$(".location-container").animate({"opacity" : "0.3"}, 300);
-		$(this).addClass("active");
-		dialogAppear(dialogSelector);
-	});
+		$(this).click(function(){
+			$(".location-container").animate({"opacity" : "0.3"}, 300);
+			$(this).addClass("active");
+			dialogAppear(dialogSelector);
+		});
 
-	$(dialogSelector + " .footer #cancel").click(function(){
-		dialogDisappear(dialogSelector)
-		$(locationSelector).removeClass("active");
-		$(locationSelector + " input[type='checkbox']").prop("checked", false);
-	})
+		$(dialogSelector + " .footer #cancel").click(function(){
+			dialogDisappear(dialogSelector)
+			$(locationSelector).removeClass("active");
+			$(locationSelector + " input[type='checkbox']").prop("checked",
+				"false");
+		})
 
-	$(dialogSelector + " .footer #select").click(function(){
-		if (totalChecked < 3) {
-		dialogDisappear(dialogSelector)
-		$(locationSelector).addClass("active");
-		totalChecked = totalChecked + 1;}
-		else {
-			alert ("You have already selected 3 locations");
-		}
+		$(dialogSelector + " .footer #select").click(function(){
+			if (totalChecked < 3) {
+				dialogDisappear(dialogSelector)
+				$(locationSelector).addClass("active");
+				totalChecked = totalChecked + 1;
+			}
+			else
+				alert ("You have already selected 3 locations.");
 
-	})
+		})
 
 	})
 
